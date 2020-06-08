12519f23d3774cce48239aa140ac83a327aeb010
Slava Maksymchuk <mail4maxsl@gmail.com>
2020-06-08 17:32:12 +0300

    cors implemented

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression')
+const cors = require('cors')
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -23,6 +24,19 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors())
+// Access-Control-Allow-Origin *
+// api.semedomain.com, frontend at somedomain.com
+// app.use(cors({
+//   origin: 'https://somedomain.com'
+// }))
+
+//allow patch, delete requests
+app.options('*', cors())
+//app.options('/api/v1/tours/:id', cors())
+
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
