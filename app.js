81f60759427e78b466ae0d1fd13d38a41a2c48a6
Slava Maksymchuk <mail4maxsl@gmail.com>
2020-06-08 13:41:46 +0300

    Prepared for deployment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
-
+const compression = require('compression')
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -64,6 +64,9 @@ app.use(
     ]
   })
 );
+
+// will compress all the text that we send to clients
+app.use(compression())
 
 // Test middleware
 app.use((req, res, next) => {
